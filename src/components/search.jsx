a4891400dd9a396c74ca4439f0024fd985d64a28
Adiul Alam Adil <adiulalam@gmail.com>
2023-01-04 22:46:45 +0000

    cleanup

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -34,7 +34,7 @@ export const Search = () => {
 						{showFilter ? "Hide" : "Show"} Filter
 					</button>
 					<button class="px-4 py-2 bg-gray-100 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md">
-						Reset Filter
+						Search
 					</button>
 				</div>
 
