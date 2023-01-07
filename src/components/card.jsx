f6bf093794466ce3b70785bfa99fa6ded5269246
Adiul Alam Adil <adiulalam@gmail.com>
2023-01-07 03:11:05 +0000

    css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,9 @@
 export const Card = (props) => {
 	return (
 		<div className="flex flex-row flex-wrap items-center justify-evenly p-5 gap-5">
-			<div className="flex flex-col min-w-full md:min-w-min items-start border rounded-lg shadow-md md:flex-row md:max-w-xl bg-[#08C952]">
+			<div className="flex flex-col md:h-72 justify-around min-w-full md:min-w-min border rounded-lg shadow-md md:flex-row md:max-w-xl bg-[#08C952]">
 				<img
-					className="object-cover w-full rounded-t-lg h-96 md:h-64 md:w-48 md:rounded-none md:rounded-l-lg"
+					className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 md:rounded-none md:rounded-l-lg"
 					src={props?.image}
 					alt={props?.name}
 				/>
@@ -22,31 +22,34 @@ export const Card = (props) => {
 						</svg>
 						<h3 className="mb-2 text-lg tracking-tight text-black">{`${props?.status} - ${props?.gender} - ${props?.species}`}</h3>
 					</div>
-
-					<h2 className="font-medium text-gray-800">Origin:</h2>
-					<div className="flex flex-row items-center justify-between mb-2 gap-2">
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Dimension:</b> {props?.origin?.dimension ?? "unknown"}
-						</h3>
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Type:</b> {props?.origin?.type ?? "unknown"}
-						</h3>
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Name:</b> {props?.origin?.name ?? "unknown"}
-						</h3>
+					<div>
+						<h2 className="font-medium text-gray-800">Origin:</h2>
+						<div className="flex flex-row items-center justify-between mb-2 gap-2">
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Dimension:</b> {props?.origin?.dimension ?? "unknown"}
+							</h3>
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Type:</b> {props?.origin?.type ?? "unknown"}
+							</h3>
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Name:</b> {props?.origin?.name ?? "unknown"}
+							</h3>
+						</div>
 					</div>
 
-					<h2 className="font-medium text-gray-800">Last Known Location:</h2>
-					<div className="flex flex-row items-center justify-between mb-2 gap-2">
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Dimension:</b> {props?.location?.dimension ?? "unknown"}
-						</h3>
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Type:</b> {props?.location?.type ?? "unknown"}
-						</h3>
-						<h3 className="text-sm tracking-tight text-black">
-							<b>Name:</b> {props?.location?.name ?? "unknown"}
-						</h3>
+					<div>
+						<h2 className="font-medium text-gray-800">Last Known Location:</h2>
+						<div className="flex flex-row items-center justify-between mb-2 gap-2">
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Dimension:</b> {props?.location?.dimension ?? "unknown"}
+							</h3>
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Type:</b> {props?.location?.type ?? "unknown"}
+							</h3>
+							<h3 className="text-sm tracking-tight text-black">
+								<b>Name:</b> {props?.location?.name ?? "unknown"}
+							</h3>
+						</div>
 					</div>
 				</div>
 			</div>
