724e7ee92bfcc4e94c5035086c706b0802fb55da
Adiul Alam Adil <adiulalam@gmail.com>
2023-01-08 04:35:50 +0000

    CSS

--- src/components/cardSkeleton.jsx
+++ src/components/cardSkeleton.jsx
@@ -9,7 +9,7 @@ export const CardSkeleton = ({ array }) => {
 					className="flex flex-col md:h-72 justify-around min-w-full md:min-w-min border rounded-lg shadow-md md:flex-row md:max-w-xl bg-[#08C952]"
 					key={i}
 				>
-					<Skeleton className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-64 md:rounded-none md:rounded-l-lg bg-[#b7e4f9ff]" />
+					<Skeleton className="object-cover p-1 w-full rounded-t-lg h-96 md:h-full md:w-64 md:rounded-none md:rounded-l-lg bg-[#b7e4f9ff]" />
 					<div className="flex flex-col justify-between p-4 leading-normal w-full md:w-96">
 						<Skeleton className="mb-2 text-3xl font-bold tracking-tight bg-black" />
 						<Skeleton className="mb-2 text-lg tracking-tight bg-black" />
